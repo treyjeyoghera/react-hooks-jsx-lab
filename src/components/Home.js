f6eb2a5caaa01ae6fea8465eefa3b437eb73b4ff
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-23 23:07:26 +0300

    finsh

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={{colour: "firebrick;"}}></h1>
+    {name} is a Web Developer from {city}
+  </div>;
 }
 
 export default Home;
